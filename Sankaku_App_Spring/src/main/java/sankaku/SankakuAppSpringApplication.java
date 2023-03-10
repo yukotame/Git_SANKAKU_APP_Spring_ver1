ece1b94155a1d432a738a854fae01317d378da19
yukotame <skondo.1989.11.26@gmail.com>
2023-03-10 09:12:55 +0900

    アクセルするURLのコメントを追記

--- Sankaku_App_Spring/src/main/java/sankaku/SankakuAppSpringApplication.java
+++ Sankaku_App_Spring/src/main/java/sankaku/SankakuAppSpringApplication.java
@@ -3,6 +3,8 @@ package sankaku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*   http://localhost:8081/init/menu    */
+
 @SpringBootApplication
 public class SankakuAppSpringApplication {
 
